43ed1ab0a965b1486cf6b15e86fbcc999154ef15
Catkitkatars <catkitkat.ars@yandex.ru>
2024-03-20 22:52:51 +0500

    done

--- index.php
+++ index.php
@@ -16,10 +16,15 @@ class Camera implements Searchable {
 	public function __construct(
 		private float $long,
 		private float $lat,
+		private string $title = 'camera'
 	) {}
 
 	public function searchPoints(): array {
 		return [new SearchPoint($this->long, $this->lat)];
+	}
+
+	public function render(): string {
+		return $this->title . ': '. $this->lat  . ', ' . $this->long;
 	}
 }
 
@@ -37,14 +42,26 @@ class SpeedCamera implements Searchable {
 
 		return $searchPoints;
 	}
+
+	public function render(): string {
+		$arr = [];
+		foreach($this->cameras as $camera) {
+			array_push($arr, $camera->render());
+		}
+		return implode(' -> ', $arr);
+	}
 }
 
 
 $camera1 = new Camera(1, 1);
 $camera2 = new SpeedCamera([new Camera(2, 2), new Camera(3, 3,)]);
 
-var_dump($camera1->searchPoints());
-var_dump($camera2->searchPoints());
 
+// var_dump();
 
+// foreach($camera2->searchPoints() as $point) {
+// 	var_dump($point);
+// }
 
+var_dump($camera1->render());
+var_dump($camera2->render());
